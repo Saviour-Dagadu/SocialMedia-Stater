94833a089f196321dc10e3b78040bd1ae563e748
Saviour Dagadu (Savi) <79539570+Saviour-Dagadu@users.noreply.github.com>
2022-04-10 10:21:40 +0000

    Adding changes made to src/components/ProfileCard/ProfileCard.jsx

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -3,6 +3,8 @@ import Cover from '../../img/cover.jpg'
 import Profile from '../../img/profileImage.png'
 import './ProfileCard.css'
 const ProfileCard = () => {
+
+    const ProfilePage = true;
     return (
         <div className="ProfileCard">
             <div className="ProfileImages">
@@ -27,12 +29,22 @@ const ProfileCard = () => {
                         <span>1</span>
                         <span>Followers</span>
                     </div>
+
+                    {ProfilePage && (
+                        <>
+                        <div className="vl">
+
+                        </div>
+                        <div className="follow">
+                            <span>3</span>
+                            <span>Posts</span>
+                        </div>
+                        </>
+                    )}
                 </div>
                 <hr/>
             </div>
-            <span>
-                My Profile
-            </span>
+            {ProfilePage? '': <span>My Profile</span>}
         </div>
     )
 }
