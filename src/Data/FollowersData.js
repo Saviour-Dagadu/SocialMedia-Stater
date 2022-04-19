a325cb8ee95204271441da6abee6131b3e13edc4
Saviour Dagadu (Savi) <79539570+Saviour-Dagadu@users.noreply.github.com>
2022-04-19 11:34:50 +0000

    Adding changes made to src/Data/FollowersData.js

--- src/Data/FollowersData.js
+++ src/Data/FollowersData.js
@@ -1,11 +1,11 @@
-import img1 from '../img/img1.png';
-import img2 from '../img/img2.png';
-import img3 from '../img/img3.png';
+import img1 from '../img/Alexander.jpg';
+import img2 from '../img/elton.jpg';
+import img3 from '../img/lawerh.jpg';
 import img4 from '../img/img4.jpg';
 
 export const Followers = [
     { name: 'Alexander Manu', username: 'AlexanderManu', img: img1 },
     { name: 'Elton Asamoah', username: 'EltonAsamoah', img: img2 },
-    { name: 'Ernest Kepomeh', username: 'ErnestKepomeh', img: img3 },
+    { name: 'Enoch Lawerh Akafo', username: 'EnochLawerhAkafo', img: img3 },
     { name: 'Natasha', username: 'Natasha', img: img4 },
 ];
