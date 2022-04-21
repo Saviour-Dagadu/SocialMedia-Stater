ab94e6be6f3a658fdb941a28dd1a2155a3099e75
Saviour Dagadu (Savi) <79539570+Saviour-Dagadu@users.noreply.github.com>
2022-04-21 11:35:26 +0000

    Adding changes made to src/Data/PostsData.js

--- src/Data/PostsData.js
+++ src/Data/PostsData.js
@@ -1,27 +1,27 @@
-import postPic1 from '../img/postpic1.jpg'
-import postPic2 from '../img/postpic2.jpg'
-import postPic3 from '../img/postpic3.JPG'
+import postPic1 from '../img/godfred.jpg'
+import postPic2 from '../img/collyns.jpg'
+import postPic3 from '../img/thomas.jpg'
 
 
 export const PostsData = [
     {
         img: postPic1,
-        name: 'Tzuyu',
+        name: 'Godfred Sam',
         desc: "Happy New Year all friends! #2023",
         likes: 2300,
         liked: true
     },
     {
         img: postPic2,
-        name: 'Maryam',
+        name: 'Nana Kwaku',
         desc: "Party time ;)",
         likes: 2300,
         liked: false
     },
     {
         img: postPic3,
-        name: 'Salena Gomez',
-        desc: "At Archery Festival",
+        name: 'Thomas Coker',
+        desc: "At Cape Coast Castle Beach",
         likes: 800,
         liked: false
     }
