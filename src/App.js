78d77a3905071490e61c738480a37424c91d1360
Saviour Dagadu (Savi) <79539570+Saviour-Dagadu@users.noreply.github.com>
2022-12-02 10:05:13 +0000

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css"
 import Home from "./pages/home/Home"
 import Profile from "./pages/Profile/Profile"
+import Auth from "./pages/Auth/Auth"
 function App() {
   return (
     <div className="App">
@@ -8,6 +9,7 @@ function App() {
         <div className='blur' style={{top: '36%', left: '-8rem'}}></div>
         {/* <Home/> */}
         <Profile/>
+        {/* <Auth/> */}
     </div>
   );
 }
