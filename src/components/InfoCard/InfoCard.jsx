10fc06d6285301cbb49cf46f9bcb0599816a81d3
Saviour Dagadu (Savi) <79539570+Saviour-Dagadu@users.noreply.github.com>
2022-12-03 10:10:52 +0000

    Adding chnages made to src/components/InfoCard/

---
+++ src/components/InfoCard/InfoCard.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import './InfoCard.css'
+import {UilPen} from '@iconscout/react-unicons'
+import ProfileModal from '../ProfileModal/ProfileModal'
+
+const InfoCard = () => {
+
+    const [modalOpened, setModalOpened] = useState(false)
+
+    return (
+        <div className="InfoCard">
+            <div className="InfoHead">
+                <h4>Your Info</h4>
+                <div>
+                    <UilPen width='2rem' height='1.2rem' onClick={()=>setModalOpened(true)}/>
+                    <ProfileModal modalOpened={modalOpened}  setModalOpened={setModalOpened} />
+                </div>
+            </div>
+
+            <div className="info">
+                <span>
+                    <b>Status: </b>
+                </span>
+                <span>in Relationship</span>
+            </div>
+
+            <div className="info">
+                <span>
+                    <b>Lives in: </b>
+                </span>
+                <span>Accra</span>
+            </div>
+
+            <div className="info">
+                <span>
+                    <b>Work at: </b>
+                </span>
+                <span>Sal Consult</span>
+            </div>
+
+            <button class='button logout-button'>Logout</button>
+        </div>
+    )
+}
+
+export default InfoCard
